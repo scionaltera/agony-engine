bcb91e2a53edac50ab1e46fec58268440a76e1c7
Peter Keeler <peter@bonevm.com>
2018-03-15 01:31:17 -0700

    Add JDBC based sessions.

---
+++ src/main/java/com/agonyengine/core/config/SessionConfiguration.java
@@ -0,0 +1,9 @@
+package com.agonyengine.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 86400)
+@Configuration
+public class SessionConfiguration {
+}
