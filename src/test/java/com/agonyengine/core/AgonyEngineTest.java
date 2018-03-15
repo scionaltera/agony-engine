f7cf0bdd8f91768a15f1ba040f118b87a3e9eb0e
Peter Keeler <peter@bonevm.com>
2018-03-14 21:50:56 -0700

    Exclude the right tasks.

--- src/test/java/com/agonyengine/core/AgonyEngineTest.java
+++ src/test/java/com/agonyengine/core/AgonyEngineTest.java
@@ -1,12 +1,20 @@
 package com.agonyengine.core;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 public class AgonyEngineTest {
+    private AgonyEngine engine;
+
+    @Before
+    public void setUp() {
+        engine = new AgonyEngine();
+    }
+
     @Test
     public void test() {
-        assertTrue(true);
+        assertNotNull(engine);
     }
 }
