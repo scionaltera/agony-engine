db4e6820cf4a1b34bfc79dc4a0430e9389f8458d
Peter Keeler <peter@bonevm.com>
2018-04-26 03:47:35 -0700

    Switch to a real message broker.
    * Local dev now uses an ActiveMQ Docker container so it's similar to prod.
    * Production deploy will be to AWS MQ which is ActiveMQ under the hood.
    * AWS MQ requires SSL so there is a configuration switch to toggle.
    * The name that echoes out is now the name from the PAT. That's temporary until there are real Actors. Coming soon!
    * This should enable deploying a cluster of instances for HA rather than just one at a time!

--- src/main/java/com/agonyengine/core/resource/WebSocketResource.java
+++ src/main/java/com/agonyengine/core/resource/WebSocketResource.java
@@ -1,17 +1,38 @@
 package com.agonyengine.core.resource;
 
+import com.agonyengine.core.model.actor.PlayerActorTemplate;
 import com.agonyengine.core.model.stomp.GameOutput;
 import com.agonyengine.core.model.stomp.UserInput;
+import com.agonyengine.core.repository.PlayerActorTemplateRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
 import org.springframework.stereotype.Controller;
+
+import javax.inject.Inject;
+import java.util.UUID;
 
 @Controller
 public class WebSocketResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketResource.class);
+
+    private SessionRepository sessionRepository;
+    private PlayerActorTemplateRepository playerActorTemplateRepository;
+
+    @Inject
+    public WebSocketResource(
+        SessionRepository sessionRepository,
+        PlayerActorTemplateRepository playerActorTemplateRepository) {
+
+        this.sessionRepository = sessionRepository;
+        this.playerActorTemplateRepository = playerActorTemplateRepository;
+    }
 
     @SubscribeMapping("/queue/output")
     public GameOutput onSubscribe() {
@@ -22,9 +43,21 @@ public class WebSocketResource {
 
     @MessageMapping("/input")
     @SendToUser(value = "/queue/output", broadcast = false)
-    public GameOutput onInput(UserInput input) {
+    public GameOutput onInput(UserInput input, Message<byte[]> message) {
+        Session session = getSession(message);
+        PlayerActorTemplate pat = playerActorTemplateRepository
+            .findById(UUID.fromString(session.getAttribute("actor")))
+            .orElse(null);
+
         LOGGER.info("Input: " + input.getInput());
 
-        return new GameOutput(": " + input.getInput());
+        return new GameOutput((pat != null ? pat.getGivenName() : "Someone") + ": " + input.getInput());
+    }
+
+    private Session getSession(Message<byte[]> message) {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.wrap(message);
+        String sessionId = (String)headerAccessor.getSessionAttributes().get("SPRING.SESSION.ID");
+
+        return sessionRepository.findById(sessionId);
     }
 }
