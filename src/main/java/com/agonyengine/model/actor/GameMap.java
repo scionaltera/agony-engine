84195b1afddf87c5d96719b73f7f56d4484daab3
Peter Keeler <peter@bonevm.com>
2018-07-05 23:11:03 -0700

    Basic framework for in-game maps.
    GameMap is the basic unit for sections of the world. Each GameMap is a rectangle of arbitrary size, and each "room" (in the MUD sense) is represented as a byte within the GameMap's byte array. The value of the byte will eventually correspond to a tile in a tileset that tells us what that room is.
    
    The really clever part here is that each GameMap is a single row in the database and will be primarily accessed by ID, which can be very fast. Each map will have "portals" to other maps which is how we'll build out a full sized world. It's also how we'll be able to handle containers and inventories. If you get picked up by someone you could find yourself in a single tile map representing their inventory. If you're really big and pick up a house, the house's map could break its link to whatever map it was originally on and now link to your inventory. I think we'll find this is a very flexible system that could eventually allow for vehicles, carrying other creatures, and a reasonable way to handle the fact that MUD rooms aren't all the same "size" the way roguelike tiles are.
    
    I preloaded the database with a very boring "default" map which demonstrates two things. First, a way to preload the world with hard-coded content. That first map could be a MUD school newbie area or something like that. It's also a good testing ground for the next step, which is to add actors (aka. MOBs or NPCs and players).

---
+++ src/main/java/com/agonyengine/model/actor/GameMap.java
@@ -0,0 +1,82 @@
+package com.agonyengine.model.actor;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class GameMap {
+    @Id
+    @GeneratedValue
+    @Type(type = "pg-uuid")
+    private UUID id;
+    private int width;
+    private byte[] tiles;
+
+    public GameMap() {}
+
+    public GameMap(int width, byte[] tiles) {
+        this.width = width;
+        this.tiles = tiles;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public byte[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(byte[] tiles) {
+        this.tiles = tiles;
+    }
+
+    /*
+     * Maps are rectangles of {width} tiles on the X axis, where (0, 0) is the bottom left corner. So
+     * given the following map:
+     *
+     * 0x7 0x8 0x9 (2)
+     * 0x4 0x5 0x6 (1)
+     * 0x1 0x2 0x3 (0)
+     * (0) (1) (2)
+     *
+     * setTile(2, 1, 0x0)
+     *
+     * ...changes the map to:
+     *
+     * 0x7 0x8 0x9
+     * 0x4 0x5 0x0
+     * 0x1 0x2 0x3
+     *
+     * The following methods are meant to make it easy to deal with the map in terms of coordinates rather than
+     * having to deal directly with the byte array.
+     */
+
+    public byte getTile(int x, int y) {
+        return tiles[computeIndex(x, y)];
+    }
+
+    public void setTile(int x, int y, byte value) {
+        tiles[computeIndex(x, y)] = value;
+    }
+
+    private int computeIndex(int x, int y) {
+        return y * width + x;
+    }
+}
