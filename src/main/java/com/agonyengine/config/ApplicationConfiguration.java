7abc0f9adcc2336366890c5e5a4a3040d450e89c
Peter Keeler <peter@bonevm.com>
2019-01-21 16:11:11 -0800

    Remove unused parameters

--- src/main/java/com/agonyengine/config/ApplicationConfiguration.java
+++ src/main/java/com/agonyengine/config/ApplicationConfiguration.java
@@ -1,23 +1,15 @@
 package com.agonyengine.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Date;
-import java.util.UUID;
 
 @EnableScheduling
 @Configuration
 public class ApplicationConfiguration {
     private Date bootDate = new Date();
-
-    @Value("${agonyengine.maps.default}")
-    private UUID defaultMapId;
-
-    @Value("${agonyengine.tilesets.inventory}")
-    private UUID inventoryTilesetId;
 
     @Bean(name = "applicationVersion")
     public String applicationVersion() {
@@ -27,15 +19,5 @@ public class ApplicationConfiguration {
     @Bean(name = "applicationBootDate")
     public Date applicationBootDate() {
         return bootDate;
-    }
-
-    @Bean
-    public UUID defaultMapId() {
-        return defaultMapId;
-    }
-
-    @Bean
-    public UUID inventoryTilesetId() {
-        return inventoryTilesetId;
     }
 }
